f7f7c3a1a678de06fb79f78d388d5637e786abc1
khai18i1 <48077456+khai18i1@users.noreply.github.com>
2019-03-28 12:08:46 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,50 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace baitap
+namespace Sach
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int n;
-            Console.WriteLine("Nhap so SV:");
-            n = Int32.Parse(Console.ReadLine());
-            SinhVien[] sv = new SinhVien[n];
-            for (int i = 0; i < n; i++)
-            {
-                sv[i] = new SinhVien();
-                sv[i].nhap();
-                sv[i].IN();
-            }
-            float min = sv[0].TB();
-            for (int i = 1; i < n; i++)
-            {
-                if (sv[i].TB() < min)
-                    min = sv[i].TB();
-            }
-            Console.WriteLine("Sinh vien co diem TB thap nhat la: {0}", min);
-            for (int i = 0; i < n; i++)
-            {
-                if (sv[i].masv.Contains("IT"))
-                    Console.WriteLine("Ma sv {0} co diem TB la: {1}", sv[i].masv, sv[i].TB());
-            }
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = i + 1; j < n; j++)
-                {
-                    if (sv[j].TB() < sv[i].TB())
-                    {
-                        SinhVien t = new SinhVien();
-                        t = sv[i];
-                        sv[i] = sv[j];
-                        sv[j] = t;
-                    }
-                }
-            }
-            Console.WriteLine("Diem TB sap xep theo thu tu tang dan:");
-            for (int i = 0; i < n; i++)
-                Console.WriteLine("MaSv: {0} DiemTB: {1}", sv[i].masv, sv[i].TB());
+            SachGiaoKhoa xd = new SachGiaoKhoa();
+            xd.nhap();
+            xd.In();
+            xd.tinhtien();
             Console.ReadLine();
         }
     }
---
+++ Sach.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sach
+{
+    class Sach
+    {
+        String masach;
+        String ngaynhap;
+        public double dongia;
+        public int soluong;
+        public Sach()
+        {
+            this.masach = "AC197";
+            this.ngaynhap = "28/03/2019";
+            this.dongia = 50;
+            this.soluong = 10;
+        }
+        public void nhap()
+        {
+            Console.WriteLine("Nhap ma sach :");
+            this.masach = Console.ReadLine();
+            Console.WriteLine("Ngay nhap sach :");
+            this.ngaynhap = Console.ReadLine();
+            Console.WriteLine("Nhap don gia sach :");
+            this.dongia = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so luong sach :");
+            this.soluong = Int32.Parse(Console.ReadLine());
+        }
+        public void In()
+        {
+            Console.WriteLine("Ma sach sach: {0}\nNgay Nhap sach:  {1}\nDon Gia sach: {2}\nSo Luong sach: {3}\n ", this.masach, this.ngaynhap, this.dongia, this.soluong);
+        }
+    }
+}
